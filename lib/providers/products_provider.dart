227c3049bfceaa865394037865f218c661418523
Ahmed M <sdfdsd>
2021-04-17 13:09:02 +0200

    local state vs app-wide state

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -36,10 +36,28 @@ class Products with ChangeNotifier {
     ),
   ];
 
+//   var _showFavoritesOnly=false;
+//
+// void showFavoriteOnly(){
+//   _showFavoritesOnly=true;
+//   notifyListeners();
+// }
+// void showAll(){
+//   _showFavoritesOnly=false;
+//   notifyListeners();
+// }
+
   List<Product> _items = [];
 
   List<Product> get items {
+    // if(_showFavoritesOnly){
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
     return [...items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   void addProduct() {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/providers/products_provider.dart';
 import 'package:flutter_ecommerce/widgets/products_grid.dart';
+import 'package:provider/provider.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
+    // final prodcutsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  // prodcutsContainer.showFavoriteOnly();
+                  _showOnlyFavorites = true;
+                } else {
+                  // prodcutsContainer.showAll();
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('show All'), value: FilterOptions.All),
+            ],
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
