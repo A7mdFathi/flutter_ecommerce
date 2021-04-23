793f88ed611ae397c58ad929b179862fa7cfcd48
Ahmed <a.fathi2112@gmail.com>
2021-04-23 18:27:12 +0200

    working with futures in dart

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'product.dart';
+import 'dart:convert';
 
 class Products with ChangeNotifier {
   final List<Product> _items = [
@@ -63,19 +64,33 @@ class Products with ChangeNotifier {
 
 //saving new products
   void addProduct(Product product) {
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imgUrl: product.imgUrl,
-    );
-    _items.add(newProduct);
-    // _items.insert(0, newProduct);
-    notifyListeners();
+    const url = 'https://easyorder-25c51.firebaseio.com/products.json';
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imgUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imgUrl: product.imgUrl,
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct);
+      notifyListeners();
+    });
   }
 
-  Product findById(String id) {
+   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
