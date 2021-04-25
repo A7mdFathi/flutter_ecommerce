b7543c7892ae82069144cd064f034429ce19ba8a
Ahmed <a.fathi2112@gmail.com>
2021-04-25 22:38:48 +0200

    adding hero transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -28,12 +28,15 @@ class ProductItem extends StatelessWidget {
               arguments: productProvider.id,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(
-              productProvider.imageUrl,
+          child: Hero(
+            tag: productProvider.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(
+                productProvider.imageUrl,
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
